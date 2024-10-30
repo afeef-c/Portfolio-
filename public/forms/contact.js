92d6be81ce55da1a10e15f9cf524a270717c4324
afeef-c <afeefc123@gmail.com>
2024-10-30 12:16:47 +0530

    to new branch2

--- public/forms/contact.js
+++ public/forms/contact.js
@@ -1,13 +1,12 @@
 // public/contact.js
 
-
-
 // Fetch credentials from the server
 fetch('/config')
   .then(response => response.json())
   .then(config => {
     // Initialize EmailJS with the fetched user ID
     emailjs.init(config.userID);
+    
     document.getElementById('contact-form').addEventListener('submit', function(event) {
       event.preventDefault();
 
